f36b0be10cc91637310fa10231f9750ae1f1de2c
Ranieri Aguiar <ranieri.f.aguiar@accenture.com>
2020-09-30 11:29:58 -0300

    Fix Yaml file

--- .github/scripts/test_app.sh
+++ .github/scripts/test_app.sh
@@ -2,7 +2,7 @@
 
 set -eo pipefail
 
-xcodebuild -workspace iOSTestProject.xcworkspace \
+xcodebuild -workspace iOSTestProject.xcodeproj \
             -scheme iOSTestProject\ iOS \
             -destination platform=iOS\ Simulator,OS=13.3,name=iPhone\ 11 \
             clean test | xcpretty
